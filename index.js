cc0cf8ed2b3e3926e379bc7d7d62524e7e2259ad
Nikolay Savenko <savenkons@yandex.ru>
2020-03-28 12:46:34 +0300

    Test of new phrases

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ const { json } = require('micro');
 
 // Запуск асинхронного сервиса.
 module.exports = async (req, res) => {
-
-    // Из запроса извлекаются свойства request, session и version.
     const { request, session, version } = await json(req);
+    let  answer;
+    switch(request.original_utterance){
+        case "Кто такой астроном":
+            answer = "Бессовестный человек";
+        break;
+        default:
+            answer = "Ошибка";
+        break;
+    }
 
-    // В тело ответа вставляются свойства version и session из запроса.
-    // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
     res.end(JSON.stringify(
         {
             version,
             session,
             response: {
-                // В свойстве response.text возвращается исходная реплика пользователя.
-                // Если навык был активирован без дополнительной команды,
-                // пользователю нужно сказать "Hello!".
-                text: request.original_utterance || 'Hello!',
-
-                // Свойство response.end_session возвращается со значением false,
-                // чтобы диалог не завершался.
-                end_session: false,
+                text: answer || "Не задекларировано",
+                end_session: true,
             },
         }
     ));
